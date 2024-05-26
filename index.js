6b0c7ade3b3f62fa9c88962b6ff184ed367ea060
Algo <hyorn1987@gmail.com>
2024-05-26 18:51:20 +0300

    change pawn to another figure if reaches last row

--- index.js
+++ index.js
@@ -480,6 +480,50 @@ function turn (c, row) {
         }
         isCastling = false
         board.set_figure(chooseFigure, c, 7-row )
+        if (chooseFigure.image.includes('pawn') && 7-row == (chooseFigure.isWhite? 7:0)){
+            let popupWindow = document.querySelector('.pawn')
+            popupWindow.style.display = 'flex'
+            let popupImages = popupWindow.querySelectorAll('img')
+            if (chooseFigure.isWhite) {
+                popupImages[0].src="src/white_bishop.png"
+                popupImages[1].src="src/white_queen.png"
+                popupImages[2].src="src/white_knight.png"
+                popupImages[3].src="src/white_rook.png"
+            } else {
+                popupImages[0].src="src/black_bishop.png"
+                popupImages[1].src="src/black_queen.png"
+                popupImages[2].src="src/black_knight.png"
+                popupImages[3].src="src/black_rook.png"
+            }
+            let color_set_figure = chooseFigure.isWhite
+            for (let i=0; i<popupImages.length; i++) {
+                popupImages[i].addEventListener('click', function () {
+                    
+                        let new_set_figure = null
+                        switch(i) {
+                            case 0:  // if (x === 'value1')
+                                new_set_figure = new Bishop(c, 7-row, color_set_figure)
+                                break
+                            case 1:  // if (x === 'value2')
+                                new_set_figure = new Queen(c, 7-row, color_set_figure)
+                                break
+                            case 2:  // if (x === 'value2')
+                                new_set_figure = new Knight(c, 7-row, color_set_figure)
+                                break
+                            case 3:  // if (x === 'value2')
+                                new_set_figure = new Rook(c, 7-row, color_set_figure)
+                                break
+                          }
+                          board.set_figure(new_set_figure, c, 7-row)
+                          popupWindow.style.display = 'none'
+
+                })
+            }
+
+
+        }
+
+
         chooseFigure = null
 }
 
